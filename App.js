8ff1931b1d181fe0d4c0cad82c3bb5cbdcc8ad3f
Ibnu <alyazzar14@gmail.com>
2023-09-04 16:14:06 +0700

    flex-box

--- App.js
+++ App.js
@@ -8,7 +8,8 @@ import {
     ScrollView,
     Button,
     Alert,
-    TouchableOpacity
+    TouchableOpacity,
+    YellowBox
 } from 'react-native';
 import ExampleComponent from './src/component/ExampleComponent';
 import ClassComponent from './src/component/ClassComponent';
@@ -16,69 +17,69 @@ import exampleStyle from "./src/component/ExampleStyle";
 
 export default function App() {
     return (
-        <View style={styles.container} >
-        </View>
+        <>
+            <View style={styles.header}>
+                <Text style={{
+                    fontWeight: 'bold',
+                    fontSize: 20
+                }}>Belajar Hahh </Text>
+            </View>
+
+            <View style={styles.container}>
+
+                <View style={[{ backgroundColor: 'blue' }, styles.box]}></View>
+                <View style={[{ backgroundColor: 'yellow' }, styles.box]}></View>
+                <View style={[{ backgroundColor: 'red' }, styles.box]}></View>
+                <View style={[{ backgroundColor: 'brown' }, styles.box]}></View>
+                <View style={[{ backgroundColor: 'grey' }, styles.box]}></View>
+                <View style={[{ backgroundColor: 'green' }, styles.box]}></View>
+                <View style={[{ backgroundColor: 'blueviolet' }, styles.box]}></View>
+                <View style={[{ backgroundColor: 'red' }, styles.box]}></View>
+
+                <StatusBar style='auto' />
+            </View>
+
+            <View style={styles.navBotBar}>
+                <View style={[{ backgroundColor: 'blue' }, styles.box]}></View>
+                <View style={[{ backgroundColor: 'yellow' }, styles.box]}></View>
+                <View style={[{ backgroundColor: 'red' }, styles.box]}></View>
+            </View>
+        </>
     )
 
-
-    // return (
-    //     <ScrollView>
-    //         <View style={styles.container}>
-    //             <Text>Hello, i'm on the wotah hahaha</Text>
-
-    //             <ClassComponent />
-    //             <ExampleComponent />
-    //             <ExampleComponent />
-    //             <Greeting />
-
-    //             <Text style={[styles.titleText, exampleStyle.title]}>Contoh Title from ExampleStyle style Component</Text>
-    //             <Text style={[styles.subTitle, exampleStyle.subTitle]}>Contoh SubTitle from ExampleStyle style Component</Text>
-
-    //             <View style={{ alignSelf: 'center', marginTop: 15 }}>
-    //                 <Text>Text Input</Text>
-    //                 <TextInput style={{
-    //                     borderWidth: 1,
-    //                     paddingStart: 10,
-    //                     width: 360,
-    //                     height: 50,
-    //                     borderRadius: 5,
-    //                 }} placeholder='isi dari hati' />
-    //             </View>
-
-    //             <Image source={{ uri: "https://upload.wikimedia.org/wikipedia/id/9/94/NarutoCoverTankobon1.jpg", width: 300, height: 600 }} />
-
-
-    //             <TouchableOpacity
-    //                 style={styles.button}
-    //                 onPress={() => {
-    //                     // Tindakan yang ingin Anda lakukan saat tombol ditekan
-    //                     alert('Ini Jalan Ninja ku (￣y▽￣)╭ Ohohoho.....!');
-    //                 }}
-    //             >
-    //                 <Image source={require('./assets/cover-manga-naruto.jpg')} />
-    //             </TouchableOpacity>
-
-    //             <StatusBar style="auto" backgroundColor='red' />
-    //         </View>
-    //     </ScrollView>
-    // );
 }
-
-// const Greeting = () => {
-//     return (
-//         <View>
-//             <Text>Ape lu liat liat tong?</Text>
-//         </View>
-//     )
-// }
 
 
 const styles = StyleSheet.create({
+    header: {
+        flex: 2,
+        backgroundColor: 'yellow',
+        alignItems: 'center',
+        justifyContent: 'center'
+
+    },
     container: {
-        flex: 1,
-        backgroundColor: 'white',
-        padding: 20,
+        padding: 30,
+        flex: 3,
+        flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
+        flexWrap: 'wrap',
+        alignContent: 'center',
+        maxHeight: 500
+    },
+    box: {
+        width: 100,
+        height: 100,
+        marginHorizontal: 4,
+        marginVertical: 4,
+        borderRadius: 10
+    },
+    navBotBar: {
+        flex:0, 
+        backgroundColor: 'grey',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: 'flex-end',
     },
 });
